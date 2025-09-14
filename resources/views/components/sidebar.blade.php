a9c05cd30a1988184b2fb3f94b3cbaeee4f76a68
Beggarscorp <admin@beggarscorporation.com>
2025-09-14 11:03:01 +0530

    make website layouts

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,5 +1,5 @@
-<div class="">
+<div class="bg-admin text-white w-64 min-h-screen p-6">
     <ul>
-        <li><a href="{{ route('admin.addproduct') }}">Add Product</a></li>
+        <li class="bg-admin-light"><a href="{{ route('admin.addproduct') }}">Add Product</a></li>
     </ul>
 </div>
